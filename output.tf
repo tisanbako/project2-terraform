6a06c8271866473cbf673432e63a0c42f52cf654
tisanbako <bakotisan@gmail.com>
2023-08-05 11:55:16 -0400

    adding output file

---
+++ output.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+    value = aws_vpc.myvpc.id 
+}
+
+output "pub1subnet" {
+    value = aws_subnet.public1[*].id
+  
+}
+output "pub2subnet" {
+    value = aws_subnet.public2[*].id
+  
+}
+
+output "prvt2subnet1" {
+    value = aws_subnet.private1[*].id
+}
+
+output "prvt2subnet2" {
+    value = aws_subnet.private2[*].id
+}
